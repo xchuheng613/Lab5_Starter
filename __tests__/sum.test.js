256607717fcf8f9e764cc0f3ef1a11efe4caf1d2
xichuheng <cxi@ucsd.edu>
2025-05-06 21:22:23 -0700

    Revert "testing branch: should be wrong"
    This reverts commit 90d88ecea270b6ab6f8ac0dd21bac8efd28dc945.

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,10 +1,5 @@
 // sum.test.js
-import { sum } from '../code-to-unit-test/sum';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(4);
-});
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1,2)).toBe(4);
+  // TODO
 });
